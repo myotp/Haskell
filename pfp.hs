54b1a211a7a6f09fef974192b98ab6a5976db6ff
Jia Wang <wj2016@gmail.com>
2015-04-02 22:49:26 +0200

    more examples

--- pfp.hs
+++ pfp.hs
@@ -1,3 +1,12 @@
 map0 :: (a -> b) -> [a] -> [b]
 map0 f [] = []
 map0 f (x:xs) = f x : map0 f xs
+
+-- explicit thunk
+map1 :: (a -> b) -> [a] -> [b]
+map1 f [] = []
+map1 f (x:xs) = let
+                     x'  = f x
+                     xs' = map f xs
+                in
+                     x' : xs'

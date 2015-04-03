9919e7ad829477a80b313bf41d8fe4396d30eeee
Jia Wang <wj2016@gmail.com>
2015-04-03 13:07:11 +0200

    Eval Monad, rpar, rseq

--- learn.hs
+++ learn.hs
@@ -1,3 +1,14 @@
+import Control.Parallel.Strategies
+
 divide xs = do
   return (a:as ++ b:bs)
+  where ((a:as), (b:bs)) = splitAt (length xs `div` 2) xs
+
+-- Eval Monad, rpar and rseq
+divide2 xs = runEval (divide2' xs)
+
+divide2' xs = do
+  as' <- rpar (map (+1) (a:as))
+  rseq as'
+  return (as' ++ b:bs)
   where ((a:as), (b:bs)) = splitAt (length xs `div` 2) xs

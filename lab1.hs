cfa0a673a8cc782ff7178871f66deb6e07a08a1b
Jia Wang <wj2016@gmail.com>
2015-04-04 01:39:18 +0200

    divide job into small chunks and run in parallel

--- lab1.hs
+++ lab1.hs
@@ -1,22 +1,31 @@
+import Control.Parallel
+import Control.Exception
+import Data.Time.Clock
+import Text.Printf
+import System.Environment
+
 import Control.Parallel.Strategies
 
-par_scanl1 _ [] = error "pscanl1: empty list"
-par_scanl1 f xs
-  | length xs < 4 = scanl1 f xs
-  | otherwise = do
-      let lastElement = last as
-      as ++ (map (f lastElement) bs)
-      where (as,bs) = runEval (mypscan f xs)
+par_scanl1 chunksize f xs =
+   concat (combine f (runEval (pmap (scanl1 f) (chunkdivide chunksize xs))))
 
-mypscan :: (a -> a -> a) -> [a] -> Eval ([a], [a])
-mypscan f xs = do
-   as' <- rpar (par_scanl1 f as)
-   bs' <- rpar (par_scanl1 f bs)
-   rseq as'
-   rseq bs'
-   return (as', bs')
-   where (as, bs) = splitAt (length xs `div` 2) xs
+pmap :: (a -> b) -> [a] -> Eval [b]
+pmap f [] = return []
+pmap f (a:as) = do
+  b <- rpar (f a)
+  bs <- pmap f as
+  return (b:bs)
 
-main :: IO ()
+combine :: (a-> a-> a) -> [[a]] -> [[a]]
+combine f (x:[]) = [x]
+combine f (x:y:xs) =
+   x : combine f (map (f (last x)) y : xs)
+
+chunkdivide :: Int -> [a] -> [[a]]
+chunkdivide _ [] = []
+chunkdivide n xs = take n xs : chunkdivide n (drop n xs)
+
+main :: IO()
 main = do
-  print (length ((par_scanl1 (+) [1..10000000])::[Int]))
+--  print (length (par_scanl1 100000 (+) [1..10000000]))
+  print (length (scanl1 (+) [1..10000000]))

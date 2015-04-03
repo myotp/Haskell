55c32591fc716f563452bf7f2a586f222682f64f
Jia Wang <wj2016@gmail.com>
2015-04-03 23:45:35 +0200

    to change code

--- lab1.hs
+++ lab1.hs
@@ -1,19 +1,10 @@
 import Control.Parallel.Strategies
-import Control.DeepSeq
 
-pscanl :: (a -> b -> a) -> a -> [b] -> [a]
-pscanl f q [] = [q]
-pscanl f q (x:xs) = q:pscanl f (f q x) xs
-
---pscanl1 :: Monad m => (a -> a -> a) -> [a] -> m([a], [a]
-pscanl1 _ [] = error "pscanl1: empty list"
-pscanl1 f xs = do
---  return (as ++ bs)
-  let lastElement = as !! (length as - 1)
+par_scanl1 _ [] = error "pscanl1: empty list"
+par_scanl1 f xs = do
+  let lastElement = last as
   as ++ (map (f lastElement) bs)
---  as ++ [lastElement,0,0,0] ++ bs
   where (as,bs) = runEval (mypscan f xs)
-
 
 mypscan :: (a -> a -> a) -> [a] -> Eval ([a], [a])
 mypscan f xs = do
@@ -24,6 +15,6 @@ mypscan f xs = do
   return (as', bs')
   where ((a:as), (b:bs)) = splitAt (length xs `div` 2) xs
 
-
-foo :: a -> [a] -> [a]
-foo a [] = []
+main :: IO ()
+main = do
+  print (length ((par_scanl1 (+) [1..10000000])::[Int]))

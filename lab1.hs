04b22c2a3cda71ef5481a999f0197ea2f708ae9d
Jia Wang <wj2016@gmail.com>
2015-04-04 00:06:27 +0200

    call rpar recursively

--- lab1.hs
+++ lab1.hs
@@ -1,19 +1,21 @@
 import Control.Parallel.Strategies
 
 par_scanl1 _ [] = error "pscanl1: empty list"
-par_scanl1 f xs = do
-  let lastElement = last as
-  as ++ (map (f lastElement) bs)
-  where (as,bs) = runEval (mypscan f xs)
+par_scanl1 f xs
+  | length xs < 4 = scanl1 f xs
+  | otherwise = do
+      let lastElement = last as
+      as ++ (map (f lastElement) bs)
+      where (as,bs) = runEval (mypscan f xs)
 
 mypscan :: (a -> a -> a) -> [a] -> Eval ([a], [a])
-mypscan f xs = do
-  as' <- rpar (scanl f a as)
-  bs' <- rpar (scanl f b bs)
-  rseq as'
-  rseq bs'
-  return (as', bs')
-  where ((a:as), (b:bs)) = splitAt (length xs `div` 2) xs
+mypscan f (x:xs) = do
+   as' <- rpar (par_scanl1 f as)
+   bs' <- rpar (par_scanl1 f bs)
+   rseq as'
+   rseq bs'
+   return (as', bs')
+   where (as, bs) = splitAt (length xs `div` 2) xs
 
 main :: IO ()
 main = do

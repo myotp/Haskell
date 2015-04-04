5bb7841f6fbc69166b2edca385def8a1e688bb97
Jia Wang <wj2016@gmail.com>
2015-04-04 10:53:19 +0200

    another try

---
+++ labA1.hs
@@ -0,0 +1,74 @@
+{-|
+  How to run:
+  ghc -O2 -threaded -rtsopts -eventlog labA1.hs
+  ./labA1 1 +RTS -N2 -l -RTS     ## Linear scanl1
+  ./labA1 2 +RTS -N2 -l -RTS     ## parallel scanl1
+-}
+
+import Control.Parallel
+import Control.Exception
+import Data.Time.Clock
+import Text.Printf
+import System.Environment
+
+import Control.Parallel.Strategies
+
+{-| parallel implmentation
+    divide the job into small chunks and run each chunk in parallel
+-}
+par_scanl1 f xs = par_scanl1' 4 f xs
+par_scanl1' chunksize f xs =
+   concat (combine f (runEval (pmap (scanl1 f) (chunkdivide chunksize xs))))
+
+pmap :: (a -> b) -> [a] -> Eval [b]
+pmap f [] = return []
+pmap f (a:as) = do
+  b <- rpar (f a)
+  bs <- pmap f as
+  return (b:bs)
+
+combine :: (a-> a-> a) -> [[a]] -> [[a]]
+combine f (x:[]) = [x]
+combine f (x:y:xs) =
+   x : combine f (map (f (last x)) y : xs)
+
+chunkdivide :: Int -> [a] -> [[a]]
+chunkdivide _ [] = []
+chunkdivide n xs = take n xs : chunkdivide n (drop n xs)
+
+-- Magic expensive associative operator
+-- Since (+) is a so cheap operation, our parallel program can't beat it
+-- because it will spend much more time to create small jobs in memory.
+slow_plus a b
+  | a < 0 = -1 * (slow_plus' (abs a) b)
+  | otherwise = slow_plus' a b
+
+slow_plus' a b
+  | a == 0 = b
+  | otherwise = slow_plus' (a-1) (b+1)
+
+-- default linear scanl1
+scan0 = sum (scanl1 slow_plus [10000..10500])
+
+-- parallel implementation with rpar
+scan1 = sum (par_scanl1 slow_plus [10000..10500])
+
+-- mainly copied rpar.hs from PCPH
+-- <<main
+main = do
+  [n] <- getArgs
+  let scan_fun = [scan0, scan1] !! (read n - 1)
+  t0 <- getCurrentTime
+  printTimeSince t0
+  print scan_fun
+  printTimeSince t0
+-- >>
+printTimeSince t0 = do
+  t1 <- getCurrentTime
+  printf "time: %.2fs\n" (realToFrac (diffUTCTime t1 t0) :: Double)
+
+--main :: IO()
+--main = do
+--  print (length (par_scanl1 1000 (+) [1..10000000]))
+--  print (length (scanl1 slow_plus [10000..10050]))
+

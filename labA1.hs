b796946d6b96c7cc74c2282c8eb3db8ad74fdbed
Jia Wang <wj2016@gmail.com>
2015-04-04 12:14:05 +0200

    parallel implementation

--- labA1.hs
+++ labA1.hs
@@ -1,8 +1,10 @@
 {-|
-  How to run:
-  ghc -O2 -threaded -rtsopts -eventlog labA1.hs
-  ./labA1 1 +RTS -N2 -l -RTS     ## Linear scanl1
-  ./labA1 2 +RTS -N2 -l -RTS     ## parallel scanl1
+How to run:
+ghc -O2 -threaded -rtsopts -eventlog labA1.hs
+./labA1 1 +RTS -N1 -l -RTS     ## Linear scanl1
+./labA1 2 +RTS -N2 -l -RTS     ## parallel scanl1
+./labA1 2 +RTS -N2 -l -RTS     ## parallel scanl1
+./labA1 3 +RTS -N2 -l -RTS     ## parallel scanl1
 -}
 
 import Control.Parallel
@@ -10,14 +12,31 @@ import Control.Exception
 import Data.Time.Clock
 import Text.Printf
 import System.Environment
-
 import Control.Parallel.Strategies
+
+import Control.Parallel (par, pseq)
+
+{-| parallel implmentation
+    simply recursive solution
+-}
+par_scanl1 _ [] = error "pscanl1: empty list"
+par_scanl1 _ [x] = [x]
+par_scanl1 f xs = do
+   force' as' `par` (force' bs' `pseq` (as' ++ (map (f (last as')) bs')))
+   where (as, bs) = splitAt (length xs `div` 2) xs
+         as' = par_scanl1 f as
+         bs' = par_scanl1 f bs
+
+force' :: [a] -> ()
+force' xs = go xs `pseq` ()
+    where go (_:xs) = go xs
+          go [] = 1
 
 {-| parallel implmentation
     divide the job into small chunks and run each chunk in parallel
 -}
-par_scanl1 f xs = par_scanl1' 4 f xs
-par_scanl1' chunksize f xs =
+par_scanl2 f xs = par_scanl2' 4 f xs
+par_scanl2' chunksize f xs =
    concat (combine f (runEval (pmap (scanl1 f) (chunkdivide chunksize xs))))
 
 pmap :: (a -> b) -> [a] -> Eval [b]
@@ -45,19 +64,25 @@ slow_plus a b
 
 slow_plus' a b
   | a == 0 = b
+  | a < 0 = a + b
   | otherwise = slow_plus' (a-1) (b+1)
 
+test_data = [1000..2000]
+
 -- default linear scanl1
-scan0 = sum (scanl1 slow_plus [10000..10500])
+scan1 = sum (scanl1 slow_plus test_data)
+
+-- parallel implementation with par/pseq
+scan2 = sum (par_scanl1 slow_plus test_data)
 
 -- parallel implementation with rpar
-scan1 = sum (par_scanl1 slow_plus [10000..10500])
+scan3 = sum (par_scanl2 slow_plus test_data)
 
 -- mainly copied rpar.hs from PCPH
 -- <<main
 main = do
   [n] <- getArgs
-  let scan_fun = [scan0, scan1] !! (read n - 1)
+  let scan_fun = [scan1, scan2, scan3] !! (read n - 1)
   t0 <- getCurrentTime
   printTimeSince t0
   print scan_fun
@@ -66,9 +91,3 @@ main = do
 printTimeSince t0 = do
   t1 <- getCurrentTime
   printf "time: %.2fs\n" (realToFrac (diffUTCTime t1 t0) :: Double)
-
---main :: IO()
---main = do
---  print (length (par_scanl1 1000 (+) [1..10000000]))
---  print (length (scanl1 slow_plus [10000..10050]))
-

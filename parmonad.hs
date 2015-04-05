601a24b739e38810b2e3c662aa759fcf1fe85f8a
Jia Wang <wj2016@gmail.com>
2015-04-05 11:58:19 +0200

    9.8s with N4, some kind of parallelism in the beginning

--- parmonad.hs
+++ parmonad.hs
@@ -1,3 +1,5 @@
+import Data.Time.Clock
+import Text.Printf
 import Control.Exception
 import System.Environment
 import Control.Monad.Par.Scheds.Trace
@@ -11,7 +13,7 @@ fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 -- >>
 
-main = do
+main0 = do
   args <- getArgs
   let [n,m] = map read args
   print $
@@ -32,23 +34,43 @@ demo_par x y =
       j <- new
       fork (put i (x + y))
       fork (put j (x - y))
-      a <- get i
+      a1' <- get i
       b <- get j
-      return (a+b)
+      return (a1'+b)
 
 par_scan_simple f [a,b] =
     runPar $ do
-      i <- new
+      i1 <- new
       j <- new
-      fork (put i (scanl1 f a))
+      fork (put i1 (scanl1 f a))
       fork (put j (scanl1 f b))
-      a' <- get i
+      a' <- get i1
       b' <- get j
-      return (combine f a' b')
+      let b'' = combine f a' b'
+      return ([a', b''])
+
+pscan f [x1,x2,x3,x4] =
+  runPar $ do
+    i1 <- new
+    i2 <- new
+    i3 <- new
+    i4 <- new
+    fork (put i1 (scanl1 f x1))
+    fork (put i2 (scanl1 f x2))
+    fork (put i3 (scanl1 f x3))
+    fork (put i4 (scanl1 f x4))
+
+    x1' <- get i1
+    x2' <- get i2
+    let x2'' = combine f x1' x2'
+    x3' <- get i3
+    let x3'' = combine f x2'' x3'
+    x4' <- get i4
+    let x4'' = combine f x3'' x4'
+    return [x1', x2'', x3'', x4'']
 
 combine :: (a -> a -> a) -> [a] -> [a] -> [a]
-combine f as bs = as ++ bs'
-   where bs' = map (f (last as)) bs
+combine f as bs = map (f (last as)) bs
 
 demo_list :: [Int] -> Int
 demo_list [a, b] = a + b
@@ -57,5 +79,44 @@ demo_list [a, b, c] = a + b + c
 {-|
 :l parmonad
 par_scan_simple (+) [ [1,2], [3,4] ]
+concat (pscan (+) [ [1,2], [3,4], [5,6], [7,8] ])
+scanl1 (+) [1..8]
+chunkdivide 50 test_data
 -}
 
+test_data = [1000000..1000200]::[Int]
+--test_data = [1..8]::[Int]
+
+chunkdivide :: Int -> [a] -> [[a]]
+chunkdivide _ [] = []
+chunkdivide n xs = take n xs : chunkdivide n (drop n xs)
+
+slow_plus a b
+  | a < 0 = -1 * (slow_plus' (abs a) b 1)
+  | otherwise = slow_plus' a b 1
+
+slow_plus' a b x
+  | a == 0 = b
+  | a < 0 = a + b
+  | otherwise = slow_plus' (a-x) (b+x) x
+
+main = do
+  t0 <- getCurrentTime
+  let chunk_size = (length test_data `div` 4) + 1
+  print chunk_size
+  let chunks = chunkdivide chunk_size test_data
+  print (length chunks)
+  printTimeSince t0
+  let result = concat (pscan slow_plus chunks)
+  print (sum (result))
+  printTimeSince t0
+
+
+printTimeSince t0 = do
+  t1 <- getCurrentTime
+  printf "time: %.2fs\n" (realToFrac (diffUTCTime t1 t0) :: Double)
+
+{-|
+ghc -O2 -threaded -rtsopts -eventlog parmonad.hs
+./parmonad +RTS -N2 -s -RTS
+-}

782fa269e3b1df761f31dae668d897c3277a1d57
Jia Wang <wj2016@gmail.com>
2015-04-28 13:29:58 +0200

    it works

--- Lab2/LabB16.hs
+++ Lab2/LabB16.hs
@@ -1,35 +1,53 @@
---import Data.List
---import Data.Ord
-
 import Data.Array.Repa as Repa
 
 type BuySellResult = (Int, Int, Int)
 
-buySell :: [Int] -> BuySellResult
-buySell xs = buySell' (0, 0, 0) xs 0
+-- sequential solution
+seqBuySell :: [Int] -> BuySellResult
+seqBuySell priceList = seqBuySell' (0, 0, 0) dayIndexes priceList
+   where dayIndexes = [0..((length priceList) - 1)]
 
-buySell' :: BuySellResult -> [Int] -> Int -> BuySellResult
-buySell' result [] currentDay = result
-buySell' (b, s, maxProfit) (x:xs) currentDay =
-    if maxProfit' >= maxProfit
-        then buySell' (b', s', maxProfit') xs (currentDay+1)
-        else buySell' (b, s, maxProfit) xs (currentDay+1)
-    where (b', s', maxProfit') = findMaxProfit currentDay x xs
+seqBuySell' buySellResult [] _ = buySellResult
+seqBuySell' (b, s, p) (b2:bs) ps
+   | p' >= p = seqBuySell' (b', s', p') bs ps
+   | otherwise = seqBuySell' (b, s, p) bs ps
+   where (b', s', p') = findMaxProfitForDay ps b2
 
-findMaxProfit currentDay x [] = (0, 0, 0) -- last day
-findMaxProfit currentDay x xs =
-   (currentDay, currentDay + sellDay + 1, maxProfit)
-    where (maxPrice, sellDay) = maxi xs 0
-          maxProfit = maxPrice - x
+-- find a maximum profit with a specific buy day
+-- returns (buy_day, sell_day, profit)
+findMaxProfitForDay :: [Int] -> Int -> BuySellResult
+findMaxProfitForDay priceList buyDay
+   | (length priceList) - 1 == buyDay = (0, 0, 0) -- last day
+   | otherwise = (buyDay, sellDay, maxPrice - (priceList !! buyDay))
+   where (maxPrice, sellDay) = maxi priceList buyDay
 
-maxi [] _ = (0, 0)
-maxi (x:xs) i =
-    if x >= fst xy'
-        then (x, i)
-        else xy'
-    where xy' = maxi xs (i+1)
+maxi :: [Int] -> Int -> (Int, Int)
+maxi (x:xs) startIndex = maxi' (x, 0) startIndex $ zip xs [1..]
+
+maxi' (x, i) _ [] = (x, i)
+maxi' (x, i) i0 ((x',i'):xs)
+    | (x' > x) && (i' > i0) = maxi' (x', i') i0 xs
+    | otherwise = maxi' (x, i) i0 xs
 
 parPlus1 :: [Int] -> [Int]
 parPlus1 xs =
     toList (computeS (Repa.map (+1) a) :: Array U DIM1 Int)
     where a = fromListUnboxed (Z :. (length xs)) xs :: Array U DIM1 Int
+
+-- parallel solution
+parBuySell xs =
+   merge xs'
+   where xs' = parBuySell' xs
+
+merge (x:xs) = merge' x xs
+merge' result [] = result
+merge' (b,s,p) ((b',s',p'):xs)
+   | p' >= p = merge' (b',s',p') xs
+   | otherwise = merge' (b,s,p) xs
+
+-- parallel
+parBuySell' xs =
+    toList (computeS (Repa.map (mapf) a) :: Array U DIM1 BuySellResult)
+    where a = fromListUnboxed (Z :. (length buyDays)) buyDays :: Array U DIM1 Int
+          buyDays = [0..(length xs - 1)]
+          mapf = findMaxProfitForDay xs
